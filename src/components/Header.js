a37100f552855ca242f151d98c39efdd55dda87a
Ammon Victor <ammon.victor@gmail.com>
2021-03-10 19:42:23 +0300

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header>
+            <nav className="navbar navbar-knbs navbar-light navbar-expand-md">
+                <div className="container">
+                    <a href="/" target="_self" className="navbar-brand">Logo</a>
+                </div>
+            </nav>
+        </header>
+    )
+}
