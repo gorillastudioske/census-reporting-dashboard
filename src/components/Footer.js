6f6f67fe0a63d42fdfc3a61b620e341e64a78689
Ammon Victor <ammon.victor@gmail.com>
2021-03-10 20:50:25 +0300

    Minor update to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <footer>
-
+        <footer className="pt- text-white">
+            <div className="ft-bottom">
+                <div className="container text-center">
+                    <div className="row">
+                        <div className="col-12">
+                            <p className="my-3 text-muted">
+                                © <a href="https://gorillastudios.co.ke/">Gorilla Studios</a> | All Rights Reserved
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </footer>
     )
 }
