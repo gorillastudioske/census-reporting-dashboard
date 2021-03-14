80e54e7d5936c9c5d1bf5f830fd1c4fef6bb5e3f
Ammon Victor <ammon.victor@gmail.com>
2021-03-14 10:14:45 +0300

    Added custom loader

---
+++ src/components/MetricLoader.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function MetricLoader() {
+    return (
+        <div className="loader-wrapper">
+            <span>Loading....</span>
+            <div className="loader">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
