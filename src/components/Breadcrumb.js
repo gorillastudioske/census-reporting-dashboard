caeff61477361d5d1e7c05b828cf6ef318d3f0af
Ammon Victor <ammon.victor@gmail.com>
2021-03-19 15:16:03 +0300

    Minor update to dynamically filter region

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
-export default function Breadcrumb() {
+export default function Breadcrumb(props) {
+    let { region } = props
     return (
-        <nav className="app-breadcrumb">
+        <nav className="_app-breadcrumb">
             <ol className="breadcrumb">
-                <li className="breadcrumb-item"><a href="/" className="nuxt-link-active">Kenya</a></li>
-                <li className="breadcrumb-item">Turkana</li>
+                <li className="breadcrumb-item"><NavLink to="/" className="nuxt-link-active">Kenya</NavLink></li>
+                {region && region.name.toLocaleLowerCase() !== "kenya" ? <li className="breadcrumb-item">{region.name}</li> : null}
             </ol>
         </nav>
     )
