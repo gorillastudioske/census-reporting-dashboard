415a30b0dffb8b1113361bf75bd75f3ed0a1de9f
Ammon Victor <ammon.victor@gmail.com>
2021-04-03 18:08:36 +0300

    - Added map styles - Added geojson reference and global style reset

--- src/components/Sidebar/LeafletMap.js
+++ src/components/Sidebar/LeafletMap.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { useHistory } from 'react-router'
 // import L from 'leaflet';
 // import 'leaflet-draw';
@@ -15,25 +15,69 @@ import { InfoBox } from './InfoBox';
 
 // L.Marker.prototype.options.icon = DefaultIcon;
 
+let baseline = {
+    strokeOpacity: 0.7,
+    weight: 1,
+    strokeLinecap: "round",
+    strokeLinejoin: "round",
+    fillOpacity: 0.3,
+    fillRule: "evenodd",
+}
+const styles = {
+    default: {
+        ...baseline,
+        color: "#e2e8f0",
+        fillColor: "#cbd5e0",
+    },
+    hover: {
+        ...baseline,
+        color: "#cbd5e0",
+        fillColor: "#e2e8f0",
+    },
+    selected: {
+        ...baseline,
+        color: "#e2e8f0",
+        fillColor: "#a0aec0",
+    },
+    red: {
+        ...baseline,
+        color: "#feb2b2",
+        fillColor: "#fc8181"
+    },
+    green: {
+        ...baseline,
+        color: "#9ae6b4",
+        fillColor: "#68d391"
+    },
+    yellow: {
+        ...baseline,
+        color: "#faf089",
+        fillColor: "#f6e05e"
+    },
+};
 
 export default function LeafletMap(props) {
     let history = useHistory();
+    const geojsonRef = useRef()
 
     const [region, setRegion] = useState(props.region)
 
-    function handleLayerClick(e) {
-        // let area = 0
-        // for (let section of e.target.getLatLngs()) {
-        //     if (section.length < 2) {
-        //         section = section[0]
-        //     }
-        //     area += L.GeometryUtil.geodesicArea(section)
-        // }
-        // console.log(area / 1000000)
-
+    const handleLayerClick = (e) => {
         const _region = props.counties.find(county => county.id === e.target.feature.properties.id)
         setRegion(_region)
         history.push(`/?region=${_region.slug}`);
+    }
+    const handleLayerMouseover = (e) => { }
+    const handleLayerMouseout = (e) => { }
+
+    function onEachFeatureControl(feature, layer) {
+        const events = {
+            click: (e) => { geojsonRef.current.resetStyle(); handleLayerClick(e); layer.setStyle(styles.red) },
+            mouseover: handleLayerMouseover,
+            mouseout: handleLayerMouseout,
+        }
+
+        layer.on(events)
     }
 
     return (
@@ -42,11 +86,7 @@ export default function LeafletMap(props) {
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             /> */}
-            <GeoJSON key='my-geojson' data={props.featureCollection} onEachFeature={(feature, layer) => {
-                layer.on({
-                    click: handleLayerClick
-                })
-            }} />
+            <GeoJSON key='my-geojson' ref={geojsonRef} data={props.featureCollection} onEachFeature={onEachFeatureControl} style={styles.default} />
             <InfoBox position="bottomleft" region={region} />
         </MapContainer>
     )
