112a9dbf21e1140e63c53cd54a35d43ed2846204
Ammon Victor <ammon.victor@gmail.com>
2021-04-03 18:33:07 +0300

    Enabled global state reset via setDefaults

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -4,11 +4,17 @@ import { NavLink } from 'react-router-dom'
 export default function Breadcrumb(props) {
     let { region } = props
     return (
-        <nav className="_app-breadcrumb">
+        <nav className="app-breadcrumb">
             <ol className="breadcrumb">
-                <li className="breadcrumb-item"><NavLink to="/" className="nuxt-link-active">Kenya</NavLink></li>
+                <li className="breadcrumb-item"><NavLink to="/" onClick={props.setDefaults} className="nuxt-link-active">Kenya</NavLink></li>
                 {region && region.name.toLocaleLowerCase() !== "kenya" ? <li className="breadcrumb-item">{region.name}</li> : null}
             </ol>
+            <div className="">
+                <select>
+                    <option value="2009">2009</option>
+                    <option value="2019">2019</option>
+                </select>
+            </div>
         </nav>
     )
 }
--- src/components/MobileHeader.js
+++ src/components/MobileHeader.js
@@ -1,21 +1,37 @@
 import React from 'react'
+import { useHistory } from 'react-router'
+import { Link } from 'react-router-dom';
 
 export default function MobileHeader(props) {
+    let history = useHistory();
+
+    function handleChange(event) {
+        const { value } = event.target
+
+        if ("default" === value) {
+            props.setDefaults()
+            history.push(`/`);
+            return
+        }
+
+        history.push(`/?region=${value}`);
+    }
+
     return (
         <div className="row">
             <div className="col-md-12">
                 <div className="country-search">
                     <div className="counties-select input-group">
                         <div className="input-group-prepend">
-                            <button className="btn btn-outline-secondary nuxt-link-exact-active nuxt-link-active">Kenya</button>
+                            <Link to="/" onClick={props.setDefaults} className="btn btn-outline-secondary nuxt-link-exact-active nuxt-link-active">Kenya</Link>
                         </div>
-                        <select className="custom-select">
-                            <option value="default" disabled="disabled" defaultValue>Choose a county</option>
-                            {(props.counties.map(county => <option key={county.id} value={county.id}>{county.name}</option>))}
+                        <select className="custom-select" onChange={handleChange}>
+                            <option value="default" defaultValue>Choose a county</option>
+                            {(props.counties.map(county => <option key={county.id} value={county.slug}>{county.name}</option>))}
                         </select>
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
